83111338fef1a451d168f6f4a2a4410d32d44505
Estevão Lucas <3stevaolucas@gmail.com>
2022-01-09 10:41:54 -0300

    Adicionado a paginação, eo funcionamento da api com imagens

--- setup/settings.py
+++ setup/settings.py
@@ -138,3 +138,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 
 ]
+
+#Paginação Api
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 15
+}
